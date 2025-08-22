dce151a3b73d5a401b5efc6f8df7059aa0e81ee6
Sahil Kumar <sahilrauniyar2005@gmail.com>
2025-08-22 14:01:16 +0530

    Solved Two Sum Problem

--- src/main/java/com/sahil/blind75/arrays/TwoSum.java
+++ src/main/java/com/sahil/blind75/arrays/TwoSum.java
@@ -1,4 +1,21 @@
 package com.sahil.blind75.arrays;
 
+import java.util.HashMap;
+
 public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int n=nums.length;
+        int [] ans=new int [2];
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            int requiredSum=target-nums[i];
+            if(map.containsKey(requiredSum)){
+                ans[0]=map.get(requiredSum);
+                ans[1]=i;
+                break;
+            }
+            map.put(nums[i],i);
+        }
+        return ans;
+    }
 }
